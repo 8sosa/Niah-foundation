0b10cfe0f1834cdedd0a66ee04239ca1f6daa0f0
8sosa <emmanuel.amos@stu.cu.edu.ng>
2025-04-04 11:08:23 +0100

    update

--- src/Components/Pages/Donate.js
+++ src/Components/Pages/Donate.js
@@ -5,6 +5,23 @@ export default function Donate() {
   return (
     <>
         <Container className='greenBg1 fullHeight'>
+          <div>
+            <p className='colBody openSans'>
+            Dear NF Family and Supporters,
+
+I want to take a moment to express my heartfelt gratitude for your generosity and belief in the work we do. Your support has been instrumental in driving real change, empowering lives, creating opportunities, and building a future where everyone has a chance to thrive.
+
+Because of donors like you, we have been able provide education for underserved children, support women entrepreneurs, provide mental health resources and awareness, host medical outreaches for rural communities etc. but there is still so much more to do. Every donation, no matter the size, helps us expand our reach, touch more lives, and continue making a meaningful difference.
+
+I invite you to stand with us once again. Your contribution is more than just a donation, it is an investment in hope, transformation, and lasting impact. Together, we are building a brighter tomorrow.
+
+Thank you for believing in our mission. Thank you for making a difference.
+
+With gratitude,
+Chenaniah 
+Niah Foundation
+            </p>
+          </div>
             <h1 className='pageHeader raleway asfs'>Donate Now</h1>
             <h2 className='colHeader raleway'>BANK ACCOUNT DETAILS</h2>
             <span className='colBody openSans'>Bank Name: Access bank</span>
